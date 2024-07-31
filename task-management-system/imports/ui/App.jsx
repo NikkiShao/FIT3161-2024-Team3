dc9da7816e9b1cf2e1e2dae5760900a6f43d2aed
Cathayensis <cathayensis@qq.com>
2024-07-31 15:08:24 +1000

    015 Finish Front End of Team Creation Modal

--- task-management-system/imports/ui/App.jsx
+++ task-management-system/imports/ui/App.jsx
@@ -10,13 +10,12 @@ import RegistrationPage from "./components/pages/registration/RegistrationPage";
 import AccountCreatedPage from "./components/pages/registration/AccountCreatedPage";
 import EmailVerificationPage from "./components/pages/registration/EmailVerificationPage";
 import HomePage from "./components/pages/HomePage";
-
+import TeamCreationPage from './components/pages/team/TeamCreationPage';
 
 export const App = () => (
     <div className={""}>
         <Router>
             <Routes>
-
                 <Route path="/examples" element={<ExamplesPage/>}/>
 
                 <Route path="/dashboard" element={<HomePage/>}/>
@@ -25,6 +24,7 @@ export const App = () => (
                 <Route path="/account-created/:username" element={<AccountCreatedPage/>}/>
                 <Route path="/verify-email/:token" element={<EmailVerificationPage/>}/>
 
+                <Route path="/team-creation" element={<TeamCreationPage/>}/>
             </Routes>
         </Router>
 
