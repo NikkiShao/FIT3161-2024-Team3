bb614378038e023c233a04e909a30082953a9a09
Cathayensis <cathayensis@qq.com>
2024-08-01 06:23:09 +1000

    014 update server

--- task-management-system/server/main.js
+++ task-management-system/server/main.js
@@ -1,8 +1,28 @@
 import { Meteor } from 'meteor/meteor';
+import '/imports/api/collections/team.js';
+import '/imports/api/methods/teams.js'; // 确保导入方法文件
+import '/imports/api/publications/team.js';
+import '/imports/api/collections/board.js';
+import '/imports/api/publications/board.js';
+import '/imports/api/methods/board.js';
+import '/imports/api/collections/user.js';
+import '/imports/api/publications/user.js';
+import '/imports/api/methods/user.js';
 
-// Here should be all the imports
+Accounts.emailTemplates.verifyEmail = {
+    subject() {
+        return "Verify Your Email Address";
+    },
+    text(user, url) {
+        let emailAddress = user.emails[0].address,
+            urlWithoutHash = url.replace('#/', ''),
+            emailBody = `To verify your email address (${emailAddress}) visit the following link:\n\n${urlWithoutHash}\n\n 
+            If you did not request this verification, please ignore this email.`;
+
+        return emailBody;
+    }
+};
 
 Meteor.startup(async () => {
-//   start up functions in the future potentially
-
+    console.log('Meteor server has started');
 });
