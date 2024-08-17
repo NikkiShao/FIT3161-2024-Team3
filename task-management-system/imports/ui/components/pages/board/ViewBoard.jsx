26e28188703e9e325e84d6abb063c5e6af294181
WBA sign <srat0019@student.monash.edu>
2024-08-17 15:53:29 +1000

    025- Made the board dynamic with adding multiple columns and removal of columns based on Board status data. Modified CSS to adapt to that as well as added authorisation to only team members who can access that page

--- task-management-system/imports/ui/components/pages/board/ViewBoard.jsx
+++ task-management-system/imports/ui/components/pages/board/ViewBoard.jsx
@@ -1,8 +1,16 @@
+/**
+ * File Description: Board's details page
+ * Updated Date: 17/08/2024
+ * Contributors: Samuel
+ * Version: 1.2
+ */
+
 import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useTracker } from 'meteor/react-meteor-data';
 import { BoardCollection } from '/imports/api/collections/board';
 import { TaskCollection } from '/imports/api/collections/task';
+import { TeamCollection } from '/imports/api/collections/team';
 import TaskCard from '/imports/ui/components/general/cards/TaskCard';
 import './ViewBoard.css';
 import { ChevronLeftIcon } from "@heroicons/react/24/outline";
@@ -11,14 +19,29 @@ const ViewBoardPage = () => {
     const { teamId, boardId } = useParams();
     const navigate = useNavigate();
 
-    // Fetching the board name
-    const { boardName } = useTracker(() => {
+    // Fetching user information
+    const userInfo = useTracker(() => Meteor.user());
+
+    // Fetching the team data
+    const { teamData, isLoading } = useTracker(() => {
+        const subscription = Meteor.subscribe('team_by_id', teamId);
+        return {
+            teamData: TeamCollection.findOne({ _id: teamId }),
+            isLoading: !subscription.ready(),
+        };
+    }, [teamId]);
+
+    // Fetching the board details, including boardStatuses
+    const { boardName, boardStatuses } = useTracker(() => {
         const subscription = Meteor.subscribe('all_boards');
         if (!subscription.ready()) {
-            return { boardName: 'Loading...' };
+            return { boardName: 'Loading...', boardStatuses: [] };
         }
         const board = BoardCollection.findOne({ _id: boardId });
-        return { boardName: board ? board.boardName : 'Board not found' };
+        return {
+            boardName: board ? board.boardName : 'Board not found',
+            boardStatuses: board ? board.boardStatuses : []
+        };
     }, [boardId]);
 
     // Fetching the tasks for the board
@@ -30,81 +53,68 @@ const ViewBoardPage = () => {
         return TaskCollection.find({ boardId }).fetch();
     }, [boardId]);
 
-    // Categorising tasks based on their status
-    const toDoTasks = tasks.filter(task => task.statusName === 'To Do');
-    const doingTasks = tasks.filter(task => task.statusName === 'Doing');
-    const doneTasks = tasks.filter(task => task.statusName === 'Done');
+    // Function to filter tasks by status
+    const filterTasksByStatus = (statusName) => {
+        return tasks.filter(task => task.statusName === statusName);
+    };
 
     const handleBackClick = () => {
         navigate(`/teams/${teamId}`);
     };
 
-    return (
-        <div className="viewboard-container">
-            <button className="btn-back team-back-button" onClick={handleBackClick}>
-                <ChevronLeftIcon strokeWidth={2} viewBox="0 0 23 23" width={20} height={20} />
-                Team
-            </button>
-            <div className="viewboard-top-right-buttons">
-                <button className="btn-light-grey">Manage Board</button>
-                <button className="btn-light-grey">View Logs</button>
-            </div>
-            <div className="viewboard-title">{boardName}</div>
-            <div className="viewboard-board">
-                <div className="viewboard-column">
-                    <div className="viewboard-column-title">To Do</div>
-                    {toDoTasks.map(task => (
-                        <TaskCard
-                            key={task._id}
-                            taskId={task._id}
-                            taskName={task.taskName}
-                            taskDeadlineDate={task.taskDeadlineDate}
-                            statusName={task.statusName}
-                            taskIsPinned={task.taskIsPinned}
-                            tagNames={task.tagNames}
-                            boardId={boardId}
-                        />
-                    ))}
+    // Check if data has loaded
+    if (!isLoading) {
+        // Check if the user is in the team
+        if (teamData && teamData.teamMembers.includes(userInfo?.emails?.[0]?.address)) {
+
+            return (
+                <div className="viewboard-container">
+                    <button className="btn-back team-back-button" onClick={handleBackClick}>
+                        <ChevronLeftIcon strokeWidth={2} viewBox="0 0 23 23" width={20} height={20} />
+                        Team
+                    </button>
+                    <div className="viewboard-top-right-buttons">
+                        <button className="btn-light-grey">Manage Board</button>
+                        <button className="btn-light-grey">View Logs</button>
+                    </div>
+                    <div className="viewboard-title"> Board: {boardName}</div>
+                    <div className="viewboard-board" style={{ display: 'flex', overflowX: 'auto' }}>
+                        {boardStatuses && boardStatuses.length > 0 ? (
+                            boardStatuses.sort((a, b) => a.statusOrder - b.statusOrder).map((status) => (
+                                <div className="viewboard-column" key={status.statusName}>
+                                    <div className="viewboard-column-title">{status.statusName}</div>
+                                    {filterTasksByStatus(status.statusName).map(task => (
+                                        <TaskCard
+                                            key={task._id}
+                                            taskId={task._id}
+                                            taskName={task.taskName}
+                                            taskDeadlineDate={task.taskDeadlineDate}
+                                            statusName={task.statusName}
+                                            taskIsPinned={task.taskIsPinned}
+                                            tagNames={task.tagNames}
+                                            boardId={boardId}
+                                        />
+                                    ))}
+                                </div>
+                            ))
+                        ) : (
+                            <div>No statuses available</div>  // Fallback if boardStatuses array is empty
+                        )}
+                    </div>
+                    <button className="viewboard-add-task-button">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="viewboard-add-icon">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                        Add Task
+                    </button>
                 </div>
-                <div className="viewboard-column">
-                    <div className="viewboard-column-title">Doing</div>
-                    {doingTasks.map(task => (
-                        <TaskCard
-                            key={task._id}
-                            taskId={task._id}
-                            taskName={task.taskName}
-                            taskDeadlineDate={task.taskDeadlineDate}
-                            statusName={task.statusName}
-                            taskIsPinned={task.taskIsPinned}
-                            tagNames={task.tagNames}
-                            boardId={boardId}
-                        />
-                    ))}
-                </div>
-                <div className="viewboard-column">
-                    <div className="viewboard-column-title">Done</div>
-                    {doneTasks.map(task => (
-                        <TaskCard
-                            key={task._id}
-                            taskId={task._id}
-                            taskName={task.taskName}
-                            taskDeadlineDate={task.taskDeadlineDate}
-                            statusName={task.statusName}
-                            taskIsPinned={task.taskIsPinned}
-                            tagNames={task.tagNames}
-                            boardId={boardId}
-                        />
-                    ))}
-                </div>
-            </div>
-            <button className="viewboard-add-task-button">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="viewboard-add-icon">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                </svg>
-                Add Task
-            </button>
-        </div>
-    );
+            );
+        } else {
+            return <div>You do not have access to this board.</div>;
+        }
+    }
+
+    return <div>Loading...</div>;
 };
 
 export default ViewBoardPage;
