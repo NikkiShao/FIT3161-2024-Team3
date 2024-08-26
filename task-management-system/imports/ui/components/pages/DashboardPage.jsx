7f4a27eae69f8c8c9d1c8eb502081e99c935eb0f
Audrey <audr0002@student.monash.edu>
2024-08-26 23:25:14 +1000

    035: move component and fixed styling

--- task-management-system/imports/ui/components/pages/DashboardPage.jsx
+++ task-management-system/imports/ui/components/pages/DashboardPage.jsx
@@ -7,18 +7,17 @@
 import React from 'react';
 import { getUserInfo } from "/imports/ui/components/util";
 import PinnedTasks from "/imports/ui/components/pages/dashboard/PinnedTasks";
-import UpcomingDeadlines from './UpcomingDeadlines.jsx';
+import UpcomingDeadlines from './dashboard/UpcomingDeadlines.jsx';
 
 export const DashboardPage = () => {
     const userInfo = getUserInfo();
 
     return (
         <div className="dashboard-pinned-container">
+            <UpcomingDeadlines></UpcomingDeadlines>
             <PinnedTasks userInfo={userInfo} />
             {/* You can add more components here if needed */}
-            <UpcomingDeadlines></UpcomingDeadlines>
         </div>
-        
     );
 };
 
